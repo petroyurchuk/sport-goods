ef448e27e5c00d6d95ef78422a0657fd25392fc5
PetroYurchuk <petroyurchuk66@gmail.com>
2023-08-02 15:51:49 +0300

    final commit

--- src/compnonents/Adventages/ListItemAdvantages.jsx
+++
@@ -1,9 +0,0 @@
-const ListItemAdvantages = ({ icon, text }) => {
-  return (
-    <div className="w-full flex justify-center items-center gap-1 md:max-w-[33%]">
-      <div>{icon}</div>
-      <div className="text-white">{text}</div>
-    </div>
-  );
-};
-export default ListItemAdvantages;
--- src/compnonents/Description/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Description = () => {
-  return <div>Description</div>;
-};
-export default Description;
