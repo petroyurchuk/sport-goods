4eff94a552bcdb095ca2965f5f2d8c0afb66c187
PetroYurchuk <petroyurchuk66@gmail.com>
2023-08-12 22:51:07 +0300

    changed background for header and titleProduct component

--- src/components/TitleProduct/index.jsx
+++ src/components/TitleProduct/index.jsx
@@ -1,6 +1,6 @@
 const TitleProduct = () => {
   return (
-    <h2 className="bg-green-600 text-white font-bold text-center text-sm  h-[70px] flex items-center justify-center md:text-2xl md:leading-[70px]">
+    <h2 className="bg-gradient-to-r from-orange-600 to-red-500 text-white font-bold text-center text-sm  h-[70px] flex items-center justify-center md:text-2xl md:leading-[70px]">
       Платформа для віджимань Push Up Rack Board
     </h2>
   );
